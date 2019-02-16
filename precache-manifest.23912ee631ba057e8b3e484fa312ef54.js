9187db2cc504fc1b3cbad7822bcf26fdfad946aa
Ricardo Canelas <hello@ricardocanelas.com>
2019-02-16 11:29:03 -0200

    Updates

---
+++ precache-manifest.23912ee631ba057e8b3e484fa312ef54.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "54cbde3a69f2a68e74ed",
+    "url": "/challenge-entria/static/js/runtime~main.54cbde3a.js"
+  },
+  {
+    "revision": "f66168dcba1886222770",
+    "url": "/challenge-entria/static/js/main.f66168dc.chunk.js"
+  },
+  {
+    "revision": "ce7495c0475d90270396",
+    "url": "/challenge-entria/static/js/1.ce7495c0.chunk.js"
+  },
+  {
+    "revision": "2352fa894144d4b9ef92a6310fa8123e",
+    "url": "/challenge-entria/index.html"
+  }
+];
